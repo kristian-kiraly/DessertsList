54441e67eb86332068ef1e8d7b466d39e2f7db8b
Kristian Kiraly <kristian.kiraly@gmail.com>
2024-08-26 18:17:42 -0700

    Initial Commit

---
+++ DessertsList/DessertsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsListApp.swift
+//  DessertsList
+//
+//  Created by Kristian Kiraly on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
